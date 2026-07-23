72d24e1f4842bafc3791b9b7aadabfd5f71d45eb
oanhng-o <oanhnguyenoanh06@gmail.com>
2026-07-23 23:12:42 +0700

    Enhance database schema with roles, orders, and order details tables; add foreign key constraints

--- shopapp-backend/database.sql
+++ shopapp-backend/database.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     fullname VARCHAR(100) DEFAULT '',
     phone_number VARCHAR(10) NOT NULL,
     address VARCHAR(200) DEFAULT '',
-    password VARCHAR(100) NOT NULL DEFAULT '',
+    password VARCHAR(100) NOT NULL DEFAULT '', --mat khau da ma hoa
     created_at datetime,
     updated_at datetime,
     is_active TINYINT DEFAULT 1,
@@ -15,13 +15,84 @@ CREATE TABLE users(
     google_account_id INT DEFAULT 0
 );
 
+ALTER TABLE users ADD COLUMN role_id INT;
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE users ADD FOREIGN KEY (role_id) REFERENCES roles(id);
+
 CREATE TABLE tokens(
     id INT PRIMARY KEY AUTO_INCREMENT,
     token VARCHAR(255) UNIQUE NOT NULL,
     token_type VARCHAR(50) NOT NULL,
-    expiration_date DATETIME, --ngay het han
+    expiration_date DATETIME,
     revoked TINYINT(1) NOT NULL,
     expired TINYINT(1) NOT NULL,
     user_id INT, --khoa ngoai
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE social_accounts(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    provider VARCHAR(20) NOT NULL COMMENT 'Ten nha social network',
+    provider_id VARCHAR(50) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    user_id INT, --khoa ngoai
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL DEFAULT '' COMMENT 'Ten danh muc. VD: do dien tu'
+);
+
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(350) NOT NULL DEFAULT '',
+    price FLOAT NOT NULL,
+    thumbnail VARCHAR(300) DEFAULT '',
+    description LONGTEXT DEFAULT '',
+    created_at datetime,
+    updated_at datetime,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE orders(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT, --khoa ngoai
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    fullname VARCHAR(100) DEFAULT '',
+    email VARCHAR(150) DEFAULT '',
+    phone_number VARCHAR(10) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    note VARCHAR(100) DEFAULT '',
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20),
+    total_money FLOAT CHECK(total_money >= 0)
+);
+
+ALTER TABLE orders ADD COLUMN shipping_method VARCHAR(100);
+ALTER TABLE orders ADD COLUMN shipping_address VARCHAR(200);
+ALTER TABLE orders ADD COLUMN shipping_date DATE;
+ALTER TABLE orders ADD COLUMN tracking_number VARCHAR(100);
+ALTER TABLE orders ADD COLUMN payment_method VARCHAR(100);
+ALTER TABLE orders ADD COLUMN active TINYINT(1);
+ALTER TABLE orders MODIFY COLUMN status ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled');
+
+CREATE TABLE order_details(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    price FLOAT CHECK(price >= 0),
+    number_of_products INT CHECK(number_of_products > 0),
+    total_money FLOAT CHECK(total_money >= 0),
+    color VARCHAR(20) DEFAULT ''
+);
+
