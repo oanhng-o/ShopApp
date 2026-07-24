2ddf838c825b6e33c1501fbf46f82408732b6c4d
oanhng-o <oanhnguyenoanh06@gmail.com>
2026-07-25 01:07:21 +0700

    Add Maven wrapper and initial project structure; implement CategoryController and DTO

---
+++ shopapp-backend/shopapp/src/main/java/com/project/shopapp/controller/CategoryController.java
@@ -0,0 +1,46 @@
+package com.project.shopapp.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.project.shopapp.dtos.CategoryDTO;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+//@Validated
+public class CategoryController {
+
+    @GetMapping("")
+    public ResponseEntity<String> getAllCategories(
+        @RequestParam("page") int page,
+        @RequestParam("limit") int limit
+    ) {
+        return ResponseEntity.ok(String.format("List of categories (Page: %d, Limit: %d)", page, limit));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryDTO categoryDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getFieldErrors().stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errors);
+        }
+        return ResponseEntity.ok("Category created: " + categoryDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long id) {
+        return ResponseEntity.ok("Category updated with ID: " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) {
+        return ResponseEntity.ok("Category deleted with ID: " + id);
+    }
+}
---
+++ shopapp-backend/shopapp/src/main/java/com/project/shopapp/dtos/CategoryDTO.java
@@ -0,0 +1,18 @@
+package com.project.shopapp.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    @NotEmpty(message = "Category name must not be empty")
+    private String name;
+}
